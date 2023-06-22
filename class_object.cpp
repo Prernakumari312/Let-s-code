9f551fdc102a5fbb11eb05d1657c3ceac10fa08c
Prerna kumari <122382178+Prernakumari312@users.noreply.github.com>
2023-06-22 12:34:17 +0530

    OOPs

---
+++ class_object.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class fruit{
+    public:
+      string name;
+      string color;
+};
+
+int main(){
+      fruit apple;
+      apple.name="Apple";
+      apple.color="Red";
+
+      cout<<apple.name<<"-"<<apple.color<<endl;
+
+      fruit *mango=new fruit();
+      mango->name="Mango";
+      mango->color="yellow";
+      cout<<mango->name<<"-"<<mango->color<<endl;
+}
