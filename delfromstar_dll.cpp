805870acb6381b9af1e55b4bd11679a828f47bc8
Prerna kumari <122382178+Prernakumari312@users.noreply.github.com>
2023-07-15 16:40:55 +0530

    doubly linked list

---
+++ delfromstar_dll.cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+      int val;
+      Node*prev;
+      Node*next;
+
+      Node(int data){
+        val=data;
+        prev=NULL;
+        next=NULL;
+
+      }
+};
+
+class doublylinkedlist{
+    public:
+    Node*head;
+    Node*tail;
+    doublylinkedlist(){
+        head=NULL;
+        tail=NULL;
+
+    }
+    void insertatend(int val){
+        Node* new_node=new Node(val);
+        if(tail==NULL){
+            head=new_node;
+            tail=new_node;
+            return;
+        }
+        tail->next=new_node;
+        new_node->prev=tail;
+        tail =new_node;
+        return;
+    }
+
+    void insertatpostion(int val,int k){
+        Node* temp=head;
+        int count =1;
+        while(count<k-1){
+            temp=temp->next;
+            count++;
+        }
+
+        Node* new_node=new Node(val);
+        new_node->next=temp->next;
+        temp->next=new_node;
+
+        new_node->prev=temp;
+        new_node->next->prev=new_node;
+
+        return ;
+    }
+
+    void delfromstart(){
+        if(head==NULL){
+            return;
+
+        }
+
+        Node*temp=head;
+        head=head->next;
+        if(head==NULL){
+            tail=NULL;
+
+        }
+
+        else{
+            head->prev=NULL;
+
+        }
+        free(temp);
+    }
+
+    void display(){
+        Node*temp=head;
+        while(temp!=NULL){
+            cout<<temp->val<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+};
+
+
+
+int main(){
+    doublylinkedlist dll;
+    dll.insertatend(1);
+    dll.insertatend(2);
+    dll.insertatend(3);
+    dll.display();
+    dll.insertatpostion(4,3);
+    dll.display();
+    dll.delfromstart();
+    dll.display();
+
+    
+    
+    return 0;
+}
