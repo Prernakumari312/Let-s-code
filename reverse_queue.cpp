b1fc1619169ae6050aaae9a126cd2495fdef3c8b
Prerna kumari <122382178+Prernakumari312@users.noreply.github.com>
2023-08-01 14:10:56 +0530

    queue

---
+++ reverse_queue.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
+
+int main(){
+    queue<int>qu;
+    qu.push(10);
+     qu.push(20);
+      qu.push(30);
+       qu.push(40);
+        qu.push(50);
+
+        stack<int>st;
+        while(! qu.empty() ){
+            st.push(qu.front());
+            qu.pop();
+
+        }
+
+        while(! st.empty()){
+            qu.push(st.top());
+            st.pop();
+
+        }
+
+        while(! qu.empty()){
+            cout<<qu.front()<<" ";
+            qu.pop();
+        }
+}
